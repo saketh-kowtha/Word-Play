7a9190feaa5240dfa91e893a08547dd1b76b71e0
Kowtha Saketh <kowtha.saketh@outlook.com>
2019-10-20 18:25:01 +0000

    Stage - 3 (game pending)

--- dict.js
+++ dict.js
@@ -1,25 +1,44 @@
-const actions = require('./src/actions')
+const fs = require('fs')
+const actions = require("./src/actions")
 
-process.stdout.write('\033c');
-let argsLength = process.argv.length
-
-
-if(argsLength < 2 || argsLength > 4){
-	console.log("Invalid arguments passed")
-	process.exit(0)
-}
-
-
-const $ = (process.argv.length > 1 && process.argv[2]) ? process.argv[2].replace(/^("|')(.*)("|')$/, '$1') : "";
+console.time('\x1b[36mProcessing Time ', 'Processing Time ' ,'\x1b[0m')
+let args = process.argv
 
 const ops = {
-	'syn' : actions.syn,
-	'ant' : actions.ant,
-	'def' : actions.def,
-	'exp' : actions.exp,
-	'' : actions.WOD,
-	'dict' :  actions.dict
+        'syn' : actions.syn,
+        'ant' : actions.ant,
+        'def' : actions.def,
+        'ex' : actions.exp,
+        'dict' :  actions.dict
 }
-ops[$]()
 
 
+if(args.length === 2){
+	actions.WOD()
+}
+else if(args.length === 3){
+	if(!ops[args[2]])
+		ops['dict']()
+}
+else if(args.length === 4){
+	if(ops[args[2]]) {
+		ops[args[2]]() 
+	}
+	else{
+ 		process.stdout.write("Invalid Arguments passed \n")
+	}
+}
+process.on('exit', function(){
+        let data
+        try
+        {
+                data = fs.readFileSync('std.txt')
+		fs.unlinkSync('std.txt')
+        }
+        catch(e){
+                data = `Problem while reading std file please read std file for output` + e
+        }
+        process.stdout.write(data.toString() + "\n")
+        //console.timeEnd("Processing Time ")
+	console.timeEnd('\x1b[36mProcessing Time ', 'Processing Time ' ,'\x1b[0m')
+})
--- src/formatter.js
+++ src/formatter.js
@@ -13,12 +13,14 @@ const formatter = (data, type) => {
 
 	else{
 		var table = new Table({wordWrap: true, colWidths:[5, process.stdout.columns - 10], head: ['Id'.bold.blue, type.bold.blue]})
-		data.forEach((e, index) => table.push([`#${index + 1}`, e.text]))
+		data.forEach((e, index) => table.push([ `#${(index + 1)+''}`.bold.grey , e.text]))
 		write(table.toString())
 	}
 }
 
 
 const write = (data) => fs.appendFileSync('std.txt', data + "\n")
+
+//const write = (data) => console.log(data)
 
 module.exports = {formatter, write}
